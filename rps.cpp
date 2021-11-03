16c9789e8bdd9facacd2055245f2c7dc27f0f23a
Johnnie Dinh <jhnny_dinh@live.com>
2021-11-03 02:02:47 -0700

    Added CPU name line and fixed spacing

--- rps.cpp
+++ rps.cpp
@@ -17,7 +17,8 @@ int cpuValue = 3;
 string rpsChoice;
 string cpuChoice;
 
-cout << "Rock-Paper-Scissors? ";
+cout << "Wanna play Rock-Paper-Scissors?" << endl;
+cout << "(Yes/Y or No/N): ";
 getline(cin, answer);
 cout << endl;
 Sleep(1000);
@@ -28,25 +29,27 @@ if (answer == "N" || answer == "No" || answer == "no" || answer == "n") {
 }
 else if (answer == "Y" || answer == "Yes" || answer == "yes" || answer == "y" || answer == "sure" || answer == "Sure") {
     willContinue = true;
-    cout << "Yay ! :D" << endl;
+    cout << "CPU: Yay ! :D" << endl;
     Sleep(1500);
-    cout << "Ready ? ?" << endl;
+    cout << "CPU: Ready ? ?" << endl;
     Sleep(1500);
     while (willContinue) {
+        cout << "CPU: ";
+        Sleep(500);
         cout << "Rock";
         cout << "...";
         Sleep(1000);
         cout << "Paper";
         cout << "...";
         Sleep(1000);
-        cout << "Scissors!!" << endl;
+        cout << "Scissors!!" << endl << endl;
         Sleep(1000);
         cout << "Choose Rock, Paper, or Scissors: ";
         cin >> rpsChoice;
+        cout << endl;
         Sleep(1000);
         
         cpuValue = rand() % 3;
-        cout << rand() % 3 << endl;
         if (cpuValue == 0) {
             cpuChoice = "Rock";
         }
@@ -61,43 +64,46 @@ else if (answer == "Y" || answer == "Yes" || answer == "yes" || answer == "y" ||
         Sleep(1000);
         cout << "Your Choice: " << rpsChoice << endl;
         Sleep(1500);
+        cout << endl;
 
         if (rpsChoice == cpuChoice) {
-            cout << "Draw!" << endl;
+            cout << "CPU: Draw!" << endl;
         }
         else if (rpsChoice == "Rock" && cpuChoice == "Paper") {
-            cout << "I won ! :)" << endl;
+            cout << "CPU: I won ! :)" << endl;
         }
         else if (rpsChoice == "Rock" && cpuChoice == "Scissors") {
-            cout << "You won... :(" << endl;
+            cout << "CPU: You won... :(" << endl;
         }
         else if (rpsChoice == "Paper" && cpuChoice == "Rock") {
-            cout << "You won ! D:" << endl;
+            cout << "CPU: You won ! D:" << endl;
         }
         else if (rpsChoice == "Paper" && cpuChoice == "Scissors") {
-            cout << "I won ! :D" << endl;
+            cout << "CPU: I won ! :D" << endl;
         }
         else if (rpsChoice == "Scissors" && cpuChoice == "Rock") {
-            cout << "I won ! >:)" << endl;
+            cout << "CPU: I won ! >:)" << endl;
         }
         else if (rpsChoice == "Scissors" && cpuChoice == "Paper") {
-            cout << "You won ? :O" << endl;
+            cout << "CPU: You won ? :O" << endl;
         }
-
+        
+        cout << endl;
         Sleep(1000);
 
         cout << "Play again? (Quit/Q or Yes/Y): ";
         cin >> answer;
+        cout << endl;
 
         if (answer == "Quit" || answer == "Q" || answer == "quit" || answer == "q") {
             willContinue = false;
             Sleep(1000);
-            cout << "Aw shucks...";
+            cout << "CPU: Aw shucks...";
             Sleep(1000);
             cout << "See you next time!" << endl;
         }
         else {
-            cout << "Okay, let's play again! :)" << endl << endl;
+            cout << "CPU: Okay, let's play again! :)" << endl << endl;
             Sleep(2000);
         }
     }
